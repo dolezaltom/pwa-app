6f6d3d6d7c2d7c50d8c5b02563ffdb5b863ef69d
Tomáš Doležal <tomas@studio123.info>
2020-01-21 10:57:28 +0100

    static cache

--- sw.js
+++ sw.js
@@ -1,3 +1,30 @@
+const staticCacheName = 'site-static';
+const assets = [
+  '/',
+  '/index.html',
+  '/js/app.js',
+  '/js/ui.js',
+  '/js/materialize.min.js',
+  '/css/styles.css',
+  '/css/materialize.min.css',
+  '/img/dish.png',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+];
+
 self.addEventListener('install', evt => {
-    console.log('service worker installed');
-  });
+  //console.log('service worker installed');
+  evt.waitUntil(
+    caches.open(staticCacheName).then(cache => {
+      console.log('caching shell assets');
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('activate', evt => {
+  //console.log('service worker activated');
+});
+
+self.addEventListener('fetch', evt => {
+  //console.log('fetch event', evt);
+});
