d249dea9d51b74357600b40df0c5786afeb2cdc1
Tomáš Doležal <tomas@studio123.info>
2020-01-23 15:00:10 +0100

    add and remove recipe

--- js/db.js
+++ js/db.js
@@ -14,7 +14,32 @@ db.collection('recipes').onSnapshot(snapshot => {
             renderRecipe(change.doc.data(), change.doc.id);
         }
         if (change.type === 'removed') {
-
+            removeRecipe(change.doc.id);
         }
     });
 });
+
+const form = document.querySelector('form');
+form.addEventListener('submit', evt => {
+    evt.preventDefault();
+
+    const recipe = {
+        title: form.title.value,
+        ingredients: form.ingredients.value
+    };
+
+    db.collection('recipes').add(recipe)
+        .catch(err => console.log(err));
+
+    form.title.value = '';
+    form.ingredients.value = '';
+});
+
+const recipeContainer = document.querySelector('.recipes');
+recipeContainer.addEventListener('click', evt => {
+  if(evt.target.tagName === 'I'){
+    const id = evt.target.getAttribute('data-id');
+    //console.log(id);
+    db.collection('recipes').doc(id).delete();
+  }
+})
--- js/ui.js
+++ js/ui.js
@@ -24,3 +24,8 @@ const renderRecipe = (data, id) => {
   `;
   recipes.innerHTML += html;
 };
+
+const removeRecipe = (id) => {
+  const recipe = document.querySelector(`.recipe[data-id=${id}]`);
+  recipe.remove();
+};
